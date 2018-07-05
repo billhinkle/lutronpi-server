bd4c313f79a8ca71bab0982e6021552c32a91300
billhinkle <35146997+billhinkle@users.noreply.github.com>
2018-07-05 15:02:37 -0400

    fixed typos in some startup specification examples

--- LutronPiServer.js
+++ LutronPiServer.js
@@ -1,3 +1,4 @@
+// LutronPiServer.js - 2018-07-05 typo corrections
 // illustrates how to start the LutronPi server with various bridge options
 // Lutron bridge ID is the serial number printed on the bottom of the unit just above the barcode
 const mainHomeDir = process.mainModule.paths[0].split('node_modules')[0].slice(0, -1);
@@ -5,19 +6,19 @@ var lutronpi = require(mainHomeDir + '/' + 'lutronpi');
 // Enter your Bridge specifications (nothing -> Lutron bridge discovery only
 
 lutronpi.startup( );	// with no parameters == Lutron bridge discovery; SmartThings hub discovery
-						// OR you can just run node lutronpi" directly... same thing.
+						// OR you can just run 'node lutronpi' directly from the CL ... same thing.
 
-// with parameters: bridge specification; SmartThings hub network address
+// with parameters: bridge specification, SmartThings hub network address  ... in the form:
 // lutronpi.startup( BRIDGE, SMARTTHINGS_IP );
 
 // Lutron bridge discovery, ST hub IP specified
 // lutronpi.startup( null, 192.168.0.100 );
 
 // one Lutron bridge specified, authentication user/password will be requested if needed; ST hub discovery
-// lutronpi.startup( {{type:"Lutron",id:"01ABCDEF", ip:"192.168.0.101" } );
+// lutronpi.startup( {type:"Lutron", id:"01ABCDEF", ip:"192.168.0.101"} );
 
 // one Lutron bridge specified with authentication user/password (not recommended!) ; ST hub discovery
-// lutronpi.startup( {{type:"Lutron",id:"01ABCDEF", ip:"192.168.0.101", auth:{user:"fred@wilma.com", password:"yabadabadoo"}} );
+// lutronpi.startup( {type:"Lutron", id:"01ABCDEF", ip:"192.168.0.101", auth:{user:"fred@wilma.com", password:"yabadabadoo"}} );
 
 // two Lutron bridges specified; ST hub network host name specified
 // const BRIDGE = [{type:"Lutron",id:"01ABCDEF", ip:"192.168.0.101"},{type:"lutron",id:"01FEDCBA",ip:"192.168.0.102"}];
